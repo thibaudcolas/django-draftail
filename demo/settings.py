6f22a9a111e07206e851d4cec024cc25b73bc0f4
Thibaud Colas <thibaudcolas@gmail.com>
2019-04-10 22:27:07 +0100

    chore(demo): add first version of a working Django site

--- demo/settings.py
+++ demo/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     "draftail",
+    "demo.demoapp",
 ]
 
 MIDDLEWARE = [
--- demo/urls.py
+++ demo/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from __future__ import absolute_import, unicode_literals
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+from .demoapp.views import demo_form_view
+
+urlpatterns = [
+    path("", demo_form_view, name="demo-form"),
+    path("admin/", admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
